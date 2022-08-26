49c835e8736de73d80db84dc94302615da0aaa2c
MK <max.kraev@gmail.com>
2022-08-26 18:26:14 +0400

    Test restrictions

--- src/handlers.py
+++ src/handlers.py
@@ -1,24 +1,26 @@
 import logging
 import vars
-from telegram import Update, Bot
+from telegram import Update, Bot, ChatMember
 from telegram.ext import ContextTypes
 
 
 # Commands
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    is_member = Bot.get_chat_member(vars.OM_USEFUL_CHAT_ID or vars.OM_FLOOD_CHAT_ID, update.effective_chat.id)
+    member = await Bot.get_chat_member(
+        chat_id=vars.OM_FLOOD_CHAT_ID,
+        user_id=update.effective_chat.id
+    )
 
-    if is_member:
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text='Привет, волк. Заполни анкету через /interview, после этого запусти казино через /match'
-        )
-
-    else:
-        await context.bot.send_message(
+    if member is None:
+        return await context.bot.send_message(
             chat_id=update.effective_chat.id,
             text='Надо быть в стае, чтобы пользоваться ботом. Сходи сюда и подпишись: https://boosty.to/m0rtymerr'
         )
+
+    return await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Алоха, волк! Расскажи немного о себе, и я подберу тебе человека из стаи'
+    )
 
 
 async def interview(update: Update, context: ContextTypes.DEFAULT_TYPE):
