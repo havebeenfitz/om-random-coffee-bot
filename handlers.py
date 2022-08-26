4526fddb5fa481b3070015257f9b0e1159c3b705
MK <max.kraev@gmail.com>
2022-08-26 16:25:28 +0400

    Update to v20

--- handlers.py
+++ handlers.py
@@ -1,36 +1,41 @@
-from telegram.ext import *
-import telegram.update
 import logging
+from telegram import Update
+from telegram.ext import ContextTypes
 
 
 # Commands
-
-def start_command(update, context):
-    update.message.reply_text(
-        'Привет, волк. Заполни анкету через /interview, после этого запусти казино через /match'
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Привет, волк. Заполни анкету через /interview, после этого запусти казино через /match"
     )
 
 
-def interview_command(update, context):
-    print()
+async def interview(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Interview. TODO"
+    )
 
 
-def match_command(update, context):
-    print()
+async def match(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Match. TODO"
+    )
 
 
-def stop_command(update, context):
-    print()
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.stopPoll()
+
 
 # Common handlers
-
-
-def handle_message(update: Updater, context: CallbackContext):
+def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = str(update.message.text).lower()
     logging.info(f'User ({update.message.chat_id}) said {text}')
 
     update.message.reply_text('Заполни анкету через /interview, запусти казино через /match')
 
 
-def error(update, context):
+def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.error(f'Update {update} caused error {context.error}')
