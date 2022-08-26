50ff008aee6be86ec8639fe92be1f0f943f9d9e4
MK <max.kraev@gmail.com>
2022-08-26 14:15:14 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
