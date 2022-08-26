f7ed4830a86dc87227859d78f80121220eb19c68
MK <max.kraev@gmail.com>
2022-08-26 16:47:50 +0400

    Fix errors

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(vars.API_KEY).build()
 
     # Add handlers
-    application.add_handler(CommandHandler('start', handlers.start_command))
-    application.add_handler(CommandHandler('interview', handlers.interview_command))
-    application.add_handler(CommandHandler('match', handlers.match_command))
-    application.add_handler(CommandHandler('stop', handlers.stop_command))
+    application.add_handler(CommandHandler('start', handlers.start))
+    application.add_handler(CommandHandler('interview', handlers.interview))
+    application.add_handler(CommandHandler('match', handlers.match))
+    application.add_handler(CommandHandler('stop', handlers.stop))
     application.add_handler(MessageHandler(filters.TEXT, handlers.handle_message))
     application.add_error_handler(handlers.error)
 
