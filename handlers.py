7fc403d61d53d00e2d29aa308399338d88c99b21
MK <max.kraev@gmail.com>
2022-08-26 16:43:47 +0400

    Fix errors

--- handlers.py
+++ handlers.py
@@ -26,16 +26,23 @@ async def match(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print()
+    print(update.message.text)
 
 
 # Common handlers
-def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = str(update.message.text).lower()
-    logging.info(f'User ({update.message.chat_id}) said {text}')
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logging.info(f'User ({update.message.chat_id}) said {update.message.text}')
 
-    update.message.reply_text('Заполни анкету через /interview, запусти казино через /match')
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text
+    )
 
 
-def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.error(f'Update {update} caused error {context.error}')
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="Error occured"
+    )
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import logging
 import handlers
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler
-from telegram.ext.filters import Text
+from telegram.ext import filters, ApplicationBuilder, CommandHandler, MessageHandler
 
 import vars
 
@@ -15,16 +14,12 @@ logging.info('Starting Bot')
 if __name__ == '__main__':
     application = ApplicationBuilder().token(vars.API_KEY).build()
 
-    # Add commands
+    # Add handlers
     application.add_handler(CommandHandler('start', handlers.start_command))
     application.add_handler(CommandHandler('interview', handlers.interview_command))
     application.add_handler(CommandHandler('match', handlers.match_command))
     application.add_handler(CommandHandler('stop', handlers.stop_command))
-
-    # Add message handler
-    application.add_handler(MessageHandler(Text, handlers.handle_message))
-
-    # Add error handler
+    application.add_handler(MessageHandler(filters.TEXT, handlers.handle_message))
     application.add_error_handler(handlers.error)
 
     application.run_polling()
